a249d954d1b02e3142966a35bb831d56cd93289a
Abhijeet Dutta <abhijeet@ucsb.edu>
2026-04-08 18:40:56 -0700

    fixed equal and hash

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -1,6 +1,8 @@
 package edu.ucsb.cs156.spring.hello;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -19,8 +21,55 @@ public class TeamTest {
        assert(team.getName().equals("test-team"));
     }
 
-   
-    // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
-    // 100% mutation coverage (all mutants timed out or killed)
+    @Test
+    public void toString_returns_correct_string() {
+        assertEquals("Team(name=test-team, members=[])", team.toString());
+    }
+
+    @Test
+    public void equals_same_object_returns_true() {
+        assertTrue(team.equals(team));
+    }
+
+    @Test
+    public void equals_same_values_returns_true() {
+        Team other = new Team("test-team");
+        assertEquals(team, other);
+    }
+
+    @Test
+    public void equals_different_class_returns_false() {
+        assertFalse(team.equals("test-team"));
+    }
+
+    @Test
+    public void equals_same_name_different_members_returns_false() {
+        Team other = new Team("test-team");
+        other.addMember("alice");
+
+        assertFalse(team.equals(other));
+    }
+
+    @Test
+    public void equals_different_name_same_members_returns_false() {
+        team.addMember("alice");
+        Team other = new Team("different-team");
+        other.addMember("alice");
+
+        assertFalse(team.equals(other));
+    }
+
+    @Test
+    public void hashCode_returns_expected_value_for_given_content() {
+        Team t = new Team();
+        t.setName("foo");
+        t.addMember("bar");
+
+        int expectedResult = 130294;
+        assertEquals(expectedResult, t.hashCode());
+    }
+
+   // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
+   // 100% mutation coverage (all mutants timed out or killed)
 
 }
